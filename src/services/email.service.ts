ab25c8396f5012e1e3fdad0ac5503f3bc1159bea
Carlos-Francisco-Garcia-Flores <20221021@uthh.edu.mx>
2024-10-24 13:39:26 -0600

    modificando la ruta de recuperacion de contraseña

--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -17,7 +17,7 @@ export class EmailService {
   }
 
   async sendPasswordResetEmail(to: string, resetToken: string) {
-    const url = `http://localhost:5173/cambiar_contraseña?token=${resetToken}`;
+    const url = `https://orangered-ape-514605.hostingersite.com/recuperar-contrasena?token=${resetToken}`;
     await this.transporter.sendMail({
       to,
       subject: '🔒 Solicitud de restablecimiento de Contraseña 🤔❓',
