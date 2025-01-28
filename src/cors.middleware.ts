6b31c2b5b1f288d1033462801b0a1590ceeb5666
Carlos-Francisco-Garcia-Flores <20221021@uthh.edu.mx>
2025-01-28 15:41:30 -0600

    Modificando env

--- src/cors.middleware.ts
+++ src/cors.middleware.ts
@@ -4,13 +4,13 @@ import { Request, Response, NextFunction } from 'express';
 @Injectable()
 export class CorsMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    const allowedOrigins = ['http://localhost:5173', 'https://beatbox.developers506.com', 'https://beatbox-blond.vercel.app'];
+    const allowedOrigins = 'http://localhost:5173/';
+   
+    // const allowedOrigins = ['http://localhost:5173/', 'https://beatbox.developers506.com', 'https://beatbox-blond.vercel.app', 'https://beat-box.com', 'https://beat-box.com:8080', 'https://beatbox.com', 'https://beatbox.com:8080'];
     const origin = req.headers.origin as string;
 
-    if (allowedOrigins.includes(origin)) {
-      res.header('Access-Control-Allow-Origin', origin);
-    }
-
+      res.header('Access-Control-Allow-Origin', allowedOrigins);
+    
 
     res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization');
